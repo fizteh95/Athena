7f9fa9403b1e26363436d89b06aa06929da29433
fizteh95 <causeLove95@yandex.ru>
2022-04-08 11:04:54 +0500

    Prepare for mutation and crossingover methods

--- modules/symbolic_genome.py
+++ modules/symbolic_genome.py
@@ -1,4 +1,5 @@
-import math  # ignore
+import copy
+import math  # noqa
 import random as r
 import typing as t
 
@@ -7,18 +8,23 @@ import symbolic as sym
 UNO_FUNCS = ["math.sin", "math.cos", "math.log2", "math.log10", "math.sqrt"]
 DUO_FUNCS = ["+", "-", "*", "/", "**"]
 
-INIT_NUMBER = 10
-PROB_LEAF_CREATE = 0.7
-# PROB_CONST_CREATE = 0.5
-PROB_VAR_CREATE = 0.5
-PROB_DUO_CREATE = 0.7
-# PROB_UNO_CREATE = 0.1
+INIT_NUMBER = 10  # количество объектов в первоначальной популяции
+PROB_LEAF_CREATE = 0.7  # вероятность создания листового элемента (константа, переменная), 1-PROB_LEAF_CREATE - функция
+PROB_VAR_CREATE = 0.5  # вероятность создания переменной, 1-PROB_VAR_CREATE - вероятность создания константы
+PROB_DUO_CREATE = (
+    0.7  # вероятность создания бинарной фукнции, 1-PROB_DUO_CREATE - создание унарной
+)
+NUM_OF_CROSSES = 100  # количество потомков в кроссинговере
+
+MUT_PROB_OF_TYPE = 0.6  # вероятность мутации 1 типа, 1-MUT_PROB_OF_TYPE - 2 тип
+MUT_CONST_TO_VAR = 0.1  # вероятность преобразования константы в переменную
+MUT_CONST_CHANGE = 0.1  # вероятность изменения константы на случайную величину
 
 
 class Population:
     def __init__(
         self,
-        values: t.List,
+        values: t.List[t.Union[int, float]],
         questions: t.List[t.Dict],
         answers: t.List,
         items: t.List = None,
@@ -120,12 +126,56 @@ class GenomeEvolution:
         self.population = Population(self.values, self.questions, self.answers)
 
     def crossingover(self, items):
-        ...
-        return []
+        """
+        Левое дерево базовое, отрезаем случайного потомка, справа берем случайного потомка и подключаем к левому
+        :param items:
+        :return:
+        """
+        new_items = []
+        for _ in range(NUM_OF_CROSSES):
+            parent_a = copy.deepcopy(r.choice(items))
+            parent_b = copy.deepcopy(r.choice(items))
+            ...
+            new_item = None
+            new_item = self.tree_shrink(new_item)
+            new_items.append(new_item)
+        return new_items
 
     def mutation(self, items, rate=0.2):
-        ...
-        return []
+        """
+        Изменения 1-го типа
+        Для констант - преобразование в переменную, изменение на случ. величину
+        Для переменных - преобразование в константу, в функцию с добавлением нод, в другую переменную
+        Для функций - включение в новую функцию как одного из потомков, вместо операции остается центральный потомок
+        либо один из двух потомков, операнды меняются местами, заменяем тип функции, заменяется класс функции
+
+        Изменения 2-го типа
+        Замена рандомного потомка на случайное дерево
+        Удаление UnoFunc как промежуточного узла
+        Назначение корнем дерева новой функции, при необходимости доращиваем потомков
+
+        :param items:
+        :param rate:
+        :return:
+        """
+        new_items = []
+        for item in items:
+            new_item = copy.deepcopy(item)
+            if r.random() < MUT_PROB_OF_TYPE:
+                ...
+            else:
+                ...
+            new_item = self.tree_shrink(new_item)
+            new_items.append(new_item)
+        return new_items
+
+    def tree_shrink(self, item):
+        """
+        Оптимизация дерева, схлопывание функций только с константами, ограничение глубины деревьев
+        :param item:
+        :return:
+        """
+        return item
 
     def evolute(self):
         count = 0
@@ -146,7 +196,9 @@ class GenomeEvolution:
 
 
 if __name__ == "__main__":
-    p = Population(["x", "y"], {"x": None, "y": None}, [1, 2, 3])
+    p = Population(
+        ["x", "y"], [{"x": 2, "y": 3}, {"x": 3, "y": 1}, {"x": 5, "y": 6}], [1, 2, 3]
+    )
     print(p.items[0])
     print(p.items[1])
     print("Done")
